528397330a68efce989d114ac4112946b960d3ef
dmitridrozdov <dm.drozdov@gmail.com>
2023-10-06 12:08:48 +1100

    Update index.ts

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -2,14 +2,16 @@
 
 import { revalidatePath } from "next/cache";
 // import Product from "../models/product.model";
-// import { connectToDB } from "../mongoose";
+import { connectToDB } from "../mongoose";
 import { scrapeAmazonProduct } from "../scraper";
-// import { getAveragePrice, getHighestPrice, getLowestPrice } from "../utils";
+import { getAveragePrice, getHighestPrice, getLowestPrice } from "../utils";
 import { User } from "@/types";
 // import { generateEmailBody, sendEmail } from "../nodemailer";
 
 export async function scrapeAndStoreProduct(productUrl: string) {
+    if(!productUrl) return;
     try {
+        connectToDB();
         const scrapedProduct = await scrapeAmazonProduct(productUrl);
     } catch (error: any) {
         throw new Error(`Failed to create/update product: ${error.message}`)
